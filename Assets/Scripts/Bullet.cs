4ce30365b26400968584dc6b37a402c99ed717c9
JeongJaeHun <ghdwp98@naver.com>
2024-01-30 09:31:49 +0900

    초기 커밋.

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 8f;
+    private Rigidbody bulletRigidbody;
+   
+    void Start()
+    {
+        bulletRigidbody=GetComponent<Rigidbody>();
+        //게임 오브젝트에서 rigidbidy컴포넌트를 찾아 할당
+        bulletRigidbody.velocity=transform.forward*speed;
+        //리지드바디의 속도 =앞쪽방향 * 이동 속력 
+        //transform은 자신의 트랜스폼 컴포넌트로 바로접근하는 변수
+        Destroy(gameObject, 3f);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="Player")
+        {
+            //상대방 게임 오브젝트에서 PlyaerController 컴포넌트 가져오기
+            PlayerController plyaerController=other.GetComponent<PlayerController>();
+
+            //상대방으로부터 playercontrolloer 컴포넌트를 가져오는 데 성공했다면
+            if(plyaerController!=null)
+            {
+                plyaerController.Die();
+            }
+
+        }
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody playerRigidbody;
+    public float speed = 8f;
+    void Start()
+    {
+        playerRigidbody=GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {   //수평축과 수직축의 입력값을 감지하여 저장 
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        //실제 이동 속도를 입력값과 이동 속력을 사용해 결정 
+        float xSpeed=xInput*speed;
+        float zSpeed=zInput*speed;
+
+        Vector3 newVelocity=new Vector3(xSpeed,0,zSpeed);
+        playerRigidbody.velocity = newVelocity;
+    }
+    public void Die()
+    {
+        //자신의 게임 오브젝트를 비활성화
+        gameObject.SetActive(false);
+    }
+}
